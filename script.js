c9b2957f5855e5d6792522db43ac03422230aba5
Xavier Foucrier <xavier.foucrier@gmail.com>
2017-08-07 12:13:24 +0200

    Click event listener
    Allows the mailto link to be clicked.

--- script.js
+++ script.js
@@ -3,5 +3,11 @@
 (function(){
 	document.addEventListener('DOMContentLoaded', function() {
 		
+		// highlight on mailto link
+		var mailto = document.querySelector('#mailto');
+		
+		mailto.addEventListener('click', function() {
+			window.location.href = 'mailto:hello@studiomotio.com';
+		});
 	});
 })();
