7df825034d7566ae2be0c7056911f818e5df9a7e
Xavier Foucrier <xavier.foucrier@gmail.com>
2017-08-08 18:07:35 +0200

    Highlight event listener
    This add a class "highlight" on the mailto link to emulate :hover css
    effect.

--- script.js
+++ script.js
@@ -3,11 +3,23 @@
 (function(){
 	document.addEventListener('DOMContentLoaded', function() {
 		
-		// highlight on mailto link
+		// click and highlight on mailto link
 		var mailto = document.querySelector('#mailto');
 		
 		mailto.addEventListener('click', function() {
 			window.location.href = 'mailto:hello@studiomotio.com';
+		});
+		
+		mailto.addEventListener('mouseover', function() {
+			if (!this.classList.contains('highlight')) {
+				this.classList.add('highlight');
+			}
+		});
+		
+		mailto.addEventListener('mouseout', function() {
+			if (this.classList.contains('highlight')) {
+				this.classList.remove('highlight');
+			}
 		});
 	});
 })();
