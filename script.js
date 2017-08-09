81155a246213d8c81f373b57cb1cd4dd8a32accf
Xavier Foucrier <xavier.foucrier@gmail.com>
2017-08-09 22:47:43 +0200

    Arrow highlight effect

--- script.js
+++ script.js
@@ -5,6 +5,7 @@
 		
 		// click and highlight on mailto link
 		var mailto = document.querySelector('#mailto');
+		var arrow = document.querySelector('#arrow');
 		
 		mailto.addEventListener('click', function() {
 			window.location.href = 'mailto:hello@studiomotio.com';
@@ -13,12 +14,14 @@
 		mailto.addEventListener('mouseover', function() {
 			if (!this.classList.contains('highlight')) {
 				this.classList.add('highlight');
+				arrow.classList.add('highlight');
 			}
 		});
 		
 		mailto.addEventListener('mouseout', function() {
 			if (this.classList.contains('highlight')) {
 				this.classList.remove('highlight');
+				arrow.classList.remove('highlight');
 			}
 		});
 	});
