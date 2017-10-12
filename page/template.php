98f36206d42f402d4c631b59037b6d058bc841fc
Xavier Foucrier <xavier.foucrier@gmail.com>
2017-10-12 12:37:50 +0200

    Small page engine

---
+++ page/template.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8" />
+		<meta name="robots" content="all" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+		<meta name="keywords" content="studio, motio, interactive, design, creative, web, motion, digital, development" />
+		<meta name="description" content="Studio de design interactif et développement web, basé à La Rochelle. Nous concevons et réalisons des solutions digitales sur mesure, dans le but d'offrir une expérience utilisateur intuitive, moderne et de qualité." />
+		<meta name="author" content="Studio Motio, Copyright (c) 2017" />
+		<meta name="geo.placename" content="La Rochelle, Charente-Maritime, France" />
+		<meta name="application-name" content="Studio Motio" />
+		<meta name="theme-color" content="#000" />
+		<title>Studio MOTIO &reg; &ndash; Design interactif et développement web à La Rochelle</title>
+		<link rel="icon" href="picture/favicon.ico" type="image/x-icon" />
+	</head>
+	<body>
+		<?php
+			require_once 'module/header.php';
+			require_once PATH;
+			require_once 'module/footer.php';
+		?>
+	</body>
+</html>
