3f20c17a749df0fe961abae09e0be0d1cb410ead
Xavier Foucrier <xavier.foucrier@gmail.com>
2017-10-12 09:05:32 +0200

    Environment constant
    Constant used to defines the current environment plateform: development
    or production.

--- configuration.php
+++ configuration.php
@@ -2,4 +2,7 @@
 	// engine configuration
 	define('DEFAULT_PAGE', 'index');
 	define('DEFAULT_LANGUAGE', 'fr');
+	
+	// defines current environment
+	define('ENVIRONMENT', substr($_SERVER['HTTP_HOST'], -4) == '.dev' ? 'dev' : 'prod');
 ?>
