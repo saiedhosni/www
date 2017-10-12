c99e58d4dc5c7e6f2de140957850f828648a2eb5
Xavier Foucrier <xavier.foucrier@gmail.com>
2017-10-12 18:45:09 +0200

    Unused constant removed

--- configuration.php
+++ configuration.php
@@ -1,7 +1,6 @@
 <?php
 	// engine configuration
 	define('DEFAULT_PAGE', 'index');
-	define('DEFAULT_LANGUAGE', 'fr');
 	
 	// defines current environment
 	define('ENVIRONMENT', substr($_SERVER['HTTP_HOST'], -4) == '.dev' ? 'dev' : 'prod');
