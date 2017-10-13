1ea4fd9d942a9dc49de120e0e657a467f6e41b46
Xavier Foucrier <xavier.foucrier@gmail.com>
2017-10-13 15:48:28 +0200

    Resets main script

--- javascript/script.js
+++ javascript/script.js
@@ -1,28 +1,5 @@
 'use strict';
 
 (function(){
-	document.addEventListener('DOMContentLoaded', function() {
-		
-		// click and highlight on mailto link
-		var mailto = document.querySelector('#mailto');
-		var arrow = document.querySelector('#arrow');
-		
-		mailto.addEventListener('click', function() {
-			window.location.href = 'mailto:hello@studiomotio.com';
-		});
-		
-		mailto.addEventListener('mouseover', function() {
-			if (!this.classList.contains('highlight')) {
-				this.classList.add('highlight');
-				arrow.classList.add('highlight');
-			}
-		});
-		
-		mailto.addEventListener('mouseout', function() {
-			if (this.classList.contains('highlight')) {
-				this.classList.remove('highlight');
-				arrow.classList.remove('highlight');
-			}
-		});
-	});
+	
 })();
