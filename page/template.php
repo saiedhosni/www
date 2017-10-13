994efe808d9fae7c6a2e1b6c9c70b16d85c55e37
Xavier Foucrier <xavier.foucrier@gmail.com>
2017-10-13 18:01:57 +0200

    Includes main stylesheet

--- page/template.php
+++ page/template.php
@@ -12,6 +12,7 @@
 		<meta name="theme-color" content="#000" />
 		<title><?php echo METADATA[PAGE][0] ?? ''; ?></title>
 		<link rel="icon" href="picture/favicon.ico" type="image/x-icon" />
+		<link type="text/css" rel="stylesheet" media="all" href="/style/style.min.css" />
 	</head>
 	<body>
 		<?php
