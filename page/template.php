052cfd28846be28dd32ecf7a1caf1c33b46ceb6b
Xavier Foucrier <xavier.foucrier@gmail.com>
2017-10-13 19:10:57 +0200

    Google analytics tag
    Sends statistic data to Google Analytics only from the production
    environment.

--- page/template.php
+++ page/template.php
@@ -13,6 +13,13 @@
 		<title><?php echo METADATA[PAGE][0] ?? ''; ?></title>
 		<link rel="icon" href="picture/favicon.ico" type="image/x-icon" />
 		<link type="text/css" rel="stylesheet" media="all" href="/style/style.min.css" />
+		<?php
+			if (ENVIRONMENT == 'prod') {
+		?>
+		<script type="text/javascript">(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create','UA-90171753-1', 'auto');ga('send','pageview');</script>
+		<?php
+			}
+		?>
 	</head>
 	<body>
 		<?php
