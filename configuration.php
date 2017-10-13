bc142d2cbb9f18e24378d7cf18d3c837451533e2
Xavier Foucrier <xavier.foucrier@gmail.com>
2017-10-13 17:57:29 +0200

    Titles and descriptions

--- configuration.php
+++ configuration.php
@@ -4,4 +4,51 @@
 	
 	// defines current environment
 	define('ENVIRONMENT', substr($_SERVER['HTTP_HOST'], -4) == '.dev' ? 'dev' : 'prod');
+	
+	// site metadata (suffix)
+	define('META_SUFFIX', ' &ndash; Studio MOTIO &reg;');
+	
+	// site metadata (title, description, keywords)
+	define('METADATA', array(
+		'fr/index' => array(
+			'Studio MOTIO &reg; &ndash; Design interactif et développement web à La Rochelle',
+			'Studio de design interactif et développement web, basé à La Rochelle. Nous concevons et réalisons des solutions digitales sur mesure, dans le but d\'offrir une expérience utilisateur intuitive, moderne et de qualité.'
+		),
+		'fr/le-studio' => array(
+			'Le studio' . META_SUFFIX,
+			'Studio motio est un studio français spécialisé dans le design interactif et le développement web, basé à La Rochelle.'
+		),
+		'fr/contact' => array(
+			'Contact' . META_SUFFIX,
+			'Envie de nous confier votre projet, lancer un nouveau site web ou construire une identité de marque mémorable, laissez-nous un petit mot. Nous serions ravis de travailler avec vous !'
+		),
+		'fr/mentions-legales' => array(
+			'Mentions légales' . META_SUFFIX,
+			'Informations techniques et mentions légales relatives au site du studio.'
+		),
+		'fr/404' => array(
+			'Oops! ' . META_SUFFIX,
+			'La page que vous recherchez est introuvable : elle a peut être été renommée, déplacée ou supprimée.'
+		),
+		'en/the-studio' => array(
+			'Studio MOTIO &reg; &ndash; Interactive design and creative web studio from La Rochelle',
+			'France-based interactive design and creative web studio, from La Rochelle. We design and build tailor-made digital solutions, to offer intuitive, modern and quality user experience.'
+		),
+		'en/the-studio' => array(
+			'The studio' . META_SUFFIX,
+			'Studio motio is a france-based interactive design and creative web studio, from La Rochelle.'
+		),
+		'en/contact' => array(
+			'Contact' . META_SUFFIX,
+			'Want to entrust us with your project, launch a new website or build a memorable brand identity, please leave us a note. We would be delighted to work with you!'
+		),
+		'en/legal-notices' => array(
+			'Legal notices' . META_SUFFIX,
+			'Technical information and legal notices about the studio website.'
+		),
+		'en/404' => array(
+			'Oops! ' . META_SUFFIX,
+			'The page you are looking for is unavailable: it may have been renamed, moved or deleted.'
+		)
+	));
 ?>
--- page/template.php
+++ page/template.php
@@ -5,12 +5,12 @@
 		<meta name="robots" content="all" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
 		<meta name="keywords" content="studio, motio, interactive, design, creative, web, motion, digital, development" />
-		<meta name="description" content="Studio de design interactif et développement web, basé à La Rochelle. Nous concevons et réalisons des solutions digitales sur mesure, dans le but d'offrir une expérience utilisateur intuitive, moderne et de qualité." />
+		<meta name="description" content="<?php echo METADATA[PAGE][1] ?? ''; ?>" />
 		<meta name="author" content="Studio Motio, Copyright (c) 2017" />
 		<meta name="geo.placename" content="La Rochelle, Charente-Maritime, France" />
 		<meta name="application-name" content="Studio Motio" />
 		<meta name="theme-color" content="#000" />
-		<title>Studio MOTIO &reg; &ndash; Design interactif et développement web à La Rochelle</title>
+		<title><?php echo METADATA[PAGE][0] ?? ''; ?></title>
 		<link rel="icon" href="picture/favicon.ico" type="image/x-icon" />
 	</head>
 	<body>
