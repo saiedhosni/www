7eaf9cc40569e5aa69c79524f391f00eb28e93f4
Xavier Foucrier <xavier.foucrier@gmail.com>
2017-10-20 16:57:38 +0200

    Base header

--- page/module/header.php
+++ page/module/header.php
@@ -1,1 +1,24 @@
-<!-- header -->
+<header class="row expanded collapse align-justify">
+	<div class="column shrink">
+    <div class="row collapse align-middle">
+      <div class="column">
+        <a href="/"><img src="/picture/studiomotio.svg" alt="Studio MOTIO" /></a>
+      </div>
+      <div class="column">
+        <nav>
+          <ul>
+            <li><a href="/le-studio" title="Le Studio">Studio</a></li>
+            <li><a href="/le-studio#nos-services" title="Nos services">Services</a></li>
+            <li><a href="/contact">Contact</a></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </div>
+	<div class="column shrink">
+    <ul class="lang">
+      <li><a href="/">Fr</a></li>
+      <li class="inactive"><a href="/en">En</a></li>
+    </ul>
+  </div>
+</header>
