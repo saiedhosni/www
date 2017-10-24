86ff5be91f74109c616f81c96eb9ad08480a709d
Xavier Foucrier <xavier.foucrier@gmail.com>
2017-10-24 12:32:31 +0200

    Big 'o'

--- page/fr/index.php
+++ page/fr/index.php
@@ -1,1 +1,3 @@
-index
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" class="motio-o">
+  <path fill="none" stroke-miterlimit="10" d="M12 50c0-21 17-38 38-38s38 17 38 38-17 38-38 38"/>
+</svg>
