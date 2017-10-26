0885abe03d9bf0e8292fcda82b1f6cfd47bfcf82
Xavier Foucrier <xavier.foucrier@gmail.com>
2017-10-26 13:38:21 +0200

    Missing titles

--- page/module/header.php
+++ page/module/header.php
@@ -9,7 +9,7 @@
           <ul>
             <li><a href="/le-studio" title="Le Studio">Studio</a></li>
             <li><a href="/le-studio#nos-services" title="Nos services">Services</a></li>
-            <li><a href="/contact">Contact</a></li>
+            <li><a href="/contact" title="Nous contacter">Contact</a></li>
           </ul>
         </nav>
       </div>
@@ -17,8 +17,8 @@
   </div>
 	<div class="column shrink">
     <ul class="lang">
-      <li><a href="/">Fr</a></li>
-      <li class="inactive"><a href="/en">En</a></li>
+      <li><a href="/" title="Français">Fr</a></li>
+      <li class="inactive"><a href="/en" title="Anglais">En</a></li>
     </ul>
   </div>
 </header>
