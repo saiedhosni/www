bc65c9c9c3a6f422dbad7679cd15073b6b4c6322
Xavier Foucrier <xavier.foucrier@gmail.com>
2017-10-27 12:02:36 +0200

    Studio introduction section

--- page/fr/le-studio.php
+++ page/fr/le-studio.php
@@ -1,1 +1,12 @@
-le-studio
+<section class="row column expanded black the-studio-intro">
+  <div class="row">
+    <div class="column large-7 large-offset-2">
+      <h1>Studio MOTIO est un studio Français<br />spécialisé dans le design interactif,<br />le développement web et basé à La Rochelle.</h1>
+    </div>
+  </div>
+  <div class="row">
+    <div class="column large-4 large-offset-6">
+      <p>Nous concevons et réalisons des solutions digitales sur mesure, dans le but d'offir une expérience utilisateur intuitive, moderne et de qualité.</p>
+    </div>
+  </div>
+</section>
