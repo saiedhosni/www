7a33262188d35ee43ff9ab7d544275e601ba6208
Xavier Foucrier <xavier.foucrier@gmail.com>
2017-11-01 10:26:05 +0100

    Home title and button link

--- page/fr/index.php
+++ page/fr/index.php
@@ -1,3 +1,12 @@
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" class="motio-o">
-  <path fill="none" stroke-miterlimit="10" d="M12 50c0-21 17-38 38-38s38 17 38 38-17 38-38 38"/>
-</svg>
+<div class="slider">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" class="motio-o">
+    <path fill="none" stroke-miterlimit="10" d="M12 50c0-21 17-38 38-38s38 17 38 38-17 38-38 38"/>
+  </svg>
+  <div>
+    <h1>Studio de design interactif et de développement web</h1>
+    <a href="le-studio#nos-services" class="button underline">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 12"><path fill="none" stroke-miterlimit="10" d="M10 5.5L5.3.8M10 5.5l-4.7 4.8M1 5.5h8.3" stroke-linecap="round"/></svg>
+      Découvrir nos services
+    </a>
+  </div>
+</div>
