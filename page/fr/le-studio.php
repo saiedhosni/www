05452f626ca0bcc23668d66359fbf0263956effa
Xavier Foucrier <xavier.foucrier@gmail.com>
2017-11-01 11:18:33 +0100

    Hard tabs
    Atom editor was converting tabs characters to spaces with the "soft
    tabs" option activated. Now using hard tabs by default.

--- page/fr/le-studio.php
+++ page/fr/le-studio.php
@@ -1,48 +1,48 @@
 <section class="row column expanded black the-studio-intro">
-  <div class="row">
-    <div class="column large-7 large-offset-2">
-      <h1>Studio MOTIO est un studio Français<br />spécialisé dans le design interactif,<br />le développement web et basé à La Rochelle.</h1>
-    </div>
-  </div>
-  <div class="row">
-    <div class="column large-4 large-offset-6">
-      <p>Nous concevons et réalisons des solutions digitales sur mesure, dans le but d'offrir une expérience utilisateur intuitive, moderne et de qualité.</p>
-    </div>
-  </div>
+	<div class="row">
+		<div class="column large-7 large-offset-2">
+			<h1>Studio MOTIO est un studio Français<br />spécialisé dans le design interactif,<br />le développement web et basé à La Rochelle.</h1>
+		</div>
+	</div>
+	<div class="row">
+		<div class="column large-4 large-offset-6">
+			<p>Nous concevons et réalisons des solutions digitales sur mesure, dans le but d'offrir une expérience utilisateur intuitive, moderne et de qualité.</p>
+		</div>
+	</div>
 </section>
 <section class="row column expanded white">
-  <div class="row">
-    <div class="column large-4 large-offset-6">
-      <h2>Le studio</h2>
-      <p>Spécialisé dans la création d’animations et micro-interactions web basées sur les principes du motion design, nous travaillons en collaboration avec des designers et des développeurs de proximité, perfectionnistes, pointilleux et soucieux du détail.</p>
-      <p>L'expérience utilisateur est au cœur de notre métier, c'est pourquoi nous utilisons des technologies graphiques de pointe, ayant déjà fait leur preuve et s'imposant aujourd'hui comme des références dans le domaine du web pour leur compatibilité, leurs performances et leur fluidité.</p>
-      <p>Nous aimons notre métier, et nous réalisons toujours tous nos projets avec passion.</p>
-    </div>
-  </div>
+	<div class="row">
+		<div class="column large-4 large-offset-6">
+			<h2>Le studio</h2>
+			<p>Spécialisé dans la création d’animations et micro-interactions web basées sur les principes du motion design, nous travaillons en collaboration avec des designers et des développeurs de proximité, perfectionnistes, pointilleux et soucieux du détail.</p>
+			<p>L'expérience utilisateur est au cœur de notre métier, c'est pourquoi nous utilisons des technologies graphiques de pointe, ayant déjà fait leur preuve et s'imposant aujourd'hui comme des références dans le domaine du web pour leur compatibilité, leurs performances et leur fluidité.</p>
+			<p>Nous aimons notre métier, et nous réalisons toujours tous nos projets avec passion.</p>
+		</div>
+	</div>
 </section>
 <section class="row column expanded black" id="nos-services">
-  <div class="row">
-    <div class="column large-4 large-offset-6">
-      <h2>Nos services</h2>
-      <dl>
-        <dt>Design</dt>
-        <dd>Logotype</dd>
-        <dd>Branding</dd>
-        <dd>Identité visuelle</dd>
-        <dd>Webdesign (UI)</dd>
-      </dl>
-      <dl>
-        <dt>Motion</dt>
-        <dd>Micro-interactions</dd>
-        <dd>Expérience utilisateur (UX)</dd>
-        <dd>Design interactif pour le web</dd>
-      </dl>
-      <dl>
-        <dt>Développement</dt>
-        <dd>Site vitrine</dd>
-        <dd>Plateforme sur mesure</dd>
-        <dd>E-Shop</dd>
-      </dl>
-    </div>
-  </div>
+	<div class="row">
+		<div class="column large-4 large-offset-6">
+			<h2>Nos services</h2>
+			<dl>
+				<dt>Design</dt>
+				<dd>Logotype</dd>
+				<dd>Branding</dd>
+				<dd>Identité visuelle</dd>
+				<dd>Webdesign (UI)</dd>
+			</dl>
+			<dl>
+				<dt>Motion</dt>
+				<dd>Micro-interactions</dd>
+				<dd>Expérience utilisateur (UX)</dd>
+				<dd>Design interactif pour le web</dd>
+			</dl>
+			<dl>
+				<dt>Développement</dt>
+				<dd>Site vitrine</dd>
+				<dd>Plateforme sur mesure</dd>
+				<dd>E-Shop</dd>
+			</dl>
+		</div>
+	</div>
 </section>
