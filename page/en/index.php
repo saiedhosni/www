0eb4babe14446ec8cb500bfc8a44bd309b5f61f4
Xavier Foucrier <xavier.foucrier@gmail.com>
2017-11-14 12:09:25 +0100

    English homepage

--- page/en/index.php
+++ page/en/index.php
@@ -1,1 +1,11 @@
-index
+<div class="slider">
+	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" class="motio-o">
+		<path fill="none" stroke-miterlimit="10" d="M12 50c0-21 17-38 38-38s38 17 38 38-17 38-38 38"/>
+	</svg>
+	<div>
+		<h1>France based interactive design and creative web studio</h1>
+		<a href="le-studio" class="button underline">
+			<svg><use xlink:href="/picture/sprite.svg#arrow-right" /></svg>Discover the studio
+		</a>
+	</div>
+</div>
