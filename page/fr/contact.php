0d6abe8eb2a41da76dcfd05002e90f866594fa69
Xavier Foucrier <xavier.foucrier@gmail.com>
2017-11-18 22:01:20 +0100

    Base contact form

--- page/fr/contact.php
+++ page/fr/contact.php
@@ -1,1 +1,28 @@
-contact
+<section class="row column expanded white">
+	<div class="row align-center">
+		<div class="column large-8">
+			<p>Envie de nous confier votre projet, lancer un nouveau site web ou construire une identité de marque mémorable, laissez-nous un petit mot. Nous serions ravis de travailler avec vous !</p>
+		</div>
+	</div>
+	<div class="row align-center">
+		<div class="column large-8">
+			<p><strong>Hello, quels sont vos besoins ?</strong></p>
+			<form method="post">
+				<textarea name="message"></textarea>
+				<button name="submit" class="underline button"><svg><use xlink:href="/picture/sprite.svg#arrow-right" /></svg>Poster ce message</button>
+			</form>
+		</div>
+	</div>
+	<div class="row align-center">
+		<div class="column large-4">
+			<p><strong>Partenariat</strong></p>
+			<p class="no-margin">Nous sommes toujours intéressés pour faire de nouvelles collaborations. Vous avez du talent et vous souhaitez le partager avec nous, rejoignez le collectif !</p>
+			<p class="no-margin"><small><a href="mailto:collective@studiomotio.com" target="_blank" class="underline">collective@studiomotio.com</a></small></p>
+		</div>
+		<div class="column large-4">
+			<p><strong>Jobs</strong></p>
+			<p class="no-margin">Le studio n'accueille pas de stagiaire pour le moment. Vous souhaitez tout de même nous montrer de quoi vous êtes capable ? Envoyez-nous votre CV !</p>
+			<p class="no-margin"><small><a href="mailto:jobs@studiomotio.com" target="_blank" class="underline">jobs@studiomotio.com</a></small></p>
+		</div>
+	</div>
+</section>
