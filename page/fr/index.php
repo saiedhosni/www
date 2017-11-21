5d168f4829c77fd7c5e44f72d1c164f2a207a1e2
Xavier Foucrier <xavier.foucrier@gmail.com>
2017-11-21 20:16:01 +0100

    Sprites the motio "o" displayed on the homepage

--- page/fr/index.php
+++ page/fr/index.php
@@ -1,7 +1,5 @@
 <div class="slider">
-	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" class="motio-o">
-		<path fill="none" stroke-miterlimit="10" d="M12 50c0-21 17-38 38-38s38 17 38 38-17 38-38 38"/>
-	</svg>
+	<svg class="motio-o-home"><use xlink:href="/picture/sprite.svg#logo-motio-o" /></svg>
 	<div>
 		<h1>Studio de design interactif et de développement web</h1>
 		<a href="le-studio" class="button underline">
