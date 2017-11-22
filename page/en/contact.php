02d79c676186d6572d971aa0cc49cc847f6c6db2
Xavier Foucrier <xavier.foucrier@gmail.com>
2017-11-22 10:57:32 +0100

    English contact page

--- page/en/contact.php
+++ page/en/contact.php
@@ -1,1 +1,33 @@
-contact
+<section class="row column expanded white">
+	<svg class="motio-o-contact"><use xlink:href="/picture/sprite.svg#logo-motio-o" /></svg>
+	<div class="row align-center">
+		<div class="column large-8">
+			<p>Want to entrust us with your project, launch a new website or build a memorable brand identity, leave us a note. We would love to work with you!</p>
+		</div>
+	</div>
+	<div class="row align-center">
+		<div class="column large-8">
+			<p><strong>Hello, what are your needs?</strong></p>
+			<form method="post">
+				<textarea required="required" autocomplete="off"></textarea>
+				<button name="submit" class="underline button post"><svg><use xlink:href="/picture/sprite.svg#arrow-right" /></svg>Post this message</button>
+				<strong class="message warning missing-mail">Don't forget to let your mail address</strong>
+				<strong class="message warning bad-request">Oops, your message could not be sent, please try again in a few moments</strong>
+				<strong class="message pending-request">Sending in progress..</strong>
+				<strong class="message message-sent">Message sent. We will answer you soon<span class="emoji">✌</span></strong>
+			</form>
+		</div>
+	</div>
+	<div class="row align-center">
+		<div class="column large-4">
+			<p><strong>Partnership</strong></p>
+			<p class="no-margin">We are always interested in making new collaborations. You have talent and you want to share it with us, join the collective!</p>
+			<p class="no-margin"><small><a href="mailto:collective@studiomotio.com" target="_blank" class="underline">collective@studiomotio.com</a></small></p>
+		</div>
+		<div class="column large-4">
+			<p><strong>Jobs</strong></p>
+			<p class="no-margin">The studio does not host trainees at the moment. Still want to show us what you are capable of? Send us your CV!</p>
+			<p class="no-margin"><small><a href="mailto:jobs@studiomotio.com" target="_blank" class="underline">jobs@studiomotio.com</a></small></p>
+		</div>
+	</div>
+</section>
