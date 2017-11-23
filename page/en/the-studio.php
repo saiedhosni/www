6f503774c33a8dc04b01bbe5db239d00e8a06e6b
Xavier Foucrier <xavier.foucrier@gmail.com>
2017-11-23 15:47:16 +0100

    Translation of the studio page

--- page/en/the-studio.php
+++ page/en/the-studio.php
@@ -1,1 +1,49 @@
-the-studio
+<svg class="motio-vertical"><use xlink:href="/picture/sprite.svg#logo-motio-vertical" /></svg>
+<section class="row column expanded black the-studio-intro">
+	<div class="row">
+		<div class="column large-6 large-offset-2">
+			<h1>Studio MOTIO is a France-based interactive design and creative web development studio, from La Rochelle.</h1>
+		</div>
+	</div>
+	<div class="row">
+		<div class="column large-4 large-offset-6">
+			<p>We design and build bespoke digital solutions to provide an intuitive, modern and high-quality user experience.</p>
+		</div>
+	</div>
+</section>
+<section class="row column expanded white">
+	<div class="row">
+		<div class="column large-4 large-offset-6">
+			<h2>The studio</h2>
+			<p>Specialized in animations and web micro-interactions based on the principles of motion design, we work in collaboration with designers and local developers, perfectionists, meticulous and detail-oriented.</p>
+			<p>The user experience is at the heart of our business, which is why we use cutting-edge graphic technologies, which have already proved their worth and are now recognized as references in the web domain for their compatibility, performance and their fluidity.</p>
+			<p>We love our job, and we always carry out all our projects with passion.</p>
+		</div>
+	</div>
+</section>
+<section class="row column expanded black">
+	<div class="row">
+		<div class="column large-4 large-offset-6">
+			<h2>Our services</h2>
+			<dl>
+				<dt>Design</dt>
+				<dd>Logotype</dd>
+				<dd>Branding</dd>
+				<dd>Visual identity</dd>
+				<dd>Webdesign (UI)</dd>
+			</dl>
+			<dl>
+				<dt>Motion</dt>
+				<dd>Micro-interactions</dd>
+				<dd>User experience (UX)</dd>
+				<dd>Interactive design for the web</dd>
+			</dl>
+			<dl>
+				<dt>Development</dt>
+				<dd>Showcase site</dd>
+				<dd>Custom tailored platform</dd>
+				<dd>E-Shop</dd>
+			</dl>
+		</div>
+	</div>
+</section>
