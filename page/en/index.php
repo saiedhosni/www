9da93f25bf9598acfc2e89da08d15b3bd40294d6
Xavier Foucrier <xavier.foucrier@gmail.com>
2017-11-23 15:21:30 +0100

    Spelling error

--- page/en/index.php
+++ page/en/index.php
@@ -1,7 +1,7 @@
 <div class="slider">
 	<svg class="motio-o-home"><use xlink:href="/picture/sprite.svg#logo-motio-o" /></svg>
 	<div>
-		<h1>France based interactive design & creative web studio</h1>
+		<h1>France-based interactive design & creative web studio</h1>
 		<a href="/en/the-studio" class="button underline">
 			<svg><use xlink:href="/picture/sprite.svg#arrow-right" /></svg>Discover the studio
 		</a>
