228ffd589a9a4d462955ce4f8741873376770c25
Xavier Foucrier <xavier.foucrier@gmail.com>
2017-11-30 12:55:06 +0100

    Base menu content

--- page/module/header.php
+++ page/module/header.php
@@ -11,9 +11,20 @@
 					<input type="checkbox" id="trigger" class="menu-trigger show-for-small-only">
 					<label for="trigger" class="menu-button show-for-small-only"><span></span></label>
 					<ul class="menu">
-						<li><a href="<?php echo LANGUAGE == 'fr' ? '/le-studio' : '/en/the-studio'; ?>" class="underline">Studio</a></li>
+						<li class="<?php echo PAGE == 'fr/le-studio' || PAGE == 'en/the-studio' ? 'active' : ''; ?>"><a href="<?php echo LANGUAGE == 'fr' ? '/le-studio' : '/en/the-studio'; ?>" class="underline">Studio</a></li>
 						<!-- <li><a href="<?php echo LANGUAGE == 'fr' ? '/nos-services' : '/en/our-services'; ?>" class="underline">Services</a></li> -->
-						<li><a href="<?php echo LANGUAGE == 'fr' ? '/contact' : '/en/contact'; ?>" class="underline">Contact</a></li>
+						<li class="<?php echo PAGE == 'fr/contact' || PAGE == 'en/contact' ? 'active' : ''; ?>"><a href="<?php echo LANGUAGE == 'fr' ? '/contact' : '/en/contact'; ?>" class="underline">Contact</a></li>
+						<li class="show-for-small-only">
+							<ul class="lang">
+								<li class="<?php echo LANGUAGE == 'fr' ? 'active' : ''; ?>"><a href="/" class="underline">Fr</a></li>
+								<li class="<?php echo LANGUAGE == 'en' ? 'active' : ''; ?>"><a href="/en" class="underline">En</a></li>
+							</ul>
+							<ul class="social">
+								<li><a href="https://www.facebook.com/studiomotio" target="_blank" class="underline"><svg><use xlink:href="/picture/sprite.svg#logo-facebook" /></svg></a></li>
+								<li><a href="https://www.twitter.com/studiomotio" target="_blank" class="underline"><svg><use xlink:href="/picture/sprite.svg#logo-twitter" /></svg></a></li>
+								<li><a href="https://www.instagram.com/studiomotio" target="_blank" class="underline"><svg><use xlink:href="/picture/sprite.svg#logo-instagram" /></svg></a></li>
+							</ul>
+						</li>
 					</ul>
 				</nav>
 			</div>
