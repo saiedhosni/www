567bd0915c9898cfa3eb3c2f3fc1f26bff2d25d8
Xavier Foucrier <xavier.foucrier@gmail.com>
2017-12-02 16:45:40 +0100

    Playground for the "hide close menu button" tween

---
+++ .motion/hide-close-menu-button.js
@@ -0,0 +1,36 @@
+'use strict';
+
+(function(){
+
+	// defines some design constants
+	const colors = {
+		base: '#000',
+		vibrant: '#00ffd3',
+		contrast: '#fff',
+		bright: '#5f5f5f',
+		warning: '#f6cc00'
+	};
+
+	// creates the curve editor
+	const curve = new MojsCurveEditor({
+		name: 'custom'
+	});
+
+	// creates the timeline
+	const timeline = new mojs.Timeline({
+		speed: 1.0,
+		delay: 0
+	});
+
+	// adds shapes to the timeline
+	timeline.add();
+
+	// creates the player
+	new MojsPlayer({
+		add: timeline,
+		isSaveState: true,
+		isPlaying: false,
+		isRepeat: false,
+		isHidden: false
+	});
+})();
