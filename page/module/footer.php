c3114065b23a0cbc51133dd9a70db4d9c6e29f9e
Xavier Foucrier <xavier.foucrier@gmail.com>
2017-12-04 14:08:55 +0100

    Responsive footer

--- page/module/footer.php
+++ page/module/footer.php
@@ -7,14 +7,14 @@
 	]) ? 'black' : 'white';
 ?>
 <footer class="row column expanded <?php echo $color; ?>">
-	<div class="row">
-		<div class="column large-4 large-offset-2">
+	<div class="row align-center">
+		<div class="column small-12 medium-5 medium-offset-0 large-4 large-offset-2">
 			<p class="no-margin"><strong><?php echo LANGUAGE == 'fr' ? 'Alors, on travaille ensemble ?' : 'So, do we work together?'; ?></strong></p>
 			<a href="<?php echo LANGUAGE == 'fr' ? '/contact' : '/en/contact'; ?>" class="underline button big">
 				<svg><use xlink:href="/picture/sprite.svg#arrow-right" /></svg><?php echo LANGUAGE == 'fr' ? 'Briefez nous !' : 'Brief us!'; ?>
 			</a>
 		</div>
-		<div class="column">
+		<div class="column small-12 medium-4 large-6">
 			<svg class="logo"><use xlink:href="/picture/sprite.svg#logo-studiomotio" /></svg>
 			<address>
 				<p class="no-margin">17000 La Rochelle<?php echo LANGUAGE == 'en' ? ', France' : ''; ?></p>
@@ -22,9 +22,9 @@
 			</address>
 		</div>
 	</div>
-	<div class="row">
-		<div class="column large-offset-6 shrink">
-			<p><small>Copyright Studio MOTIO &reg; &ndash; <?php echo LANGUAGE == 'fr' ? '<a href="/mentions-legales" class="underline">Mentions légales</a>' : '<a href="/en/legal-notices" class="underline">Legal notices</a>'; ?></small></p>
+	<div class="row align-center">
+		<div class="column medium-9 medium-offset-0 large-offset-6 large-6">
+			<p class="copyright"><small>Copyright Studio MOTIO &reg; &ndash; <?php echo LANGUAGE == 'fr' ? '<a href="/mentions-legales" class="underline">Mentions légales</a>' : '<a href="/en/legal-notices" class="underline">Legal notices</a>'; ?></small></p>
 		</div>
 	</div>
 </footer>
