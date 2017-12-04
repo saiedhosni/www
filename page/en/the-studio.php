a2bf2172cfb5fcb2d1f10b29032a3d426b9c448b
Xavier Foucrier <xavier.foucrier@gmail.com>
2017-12-04 22:38:01 +0100

    Responsive the studio page

--- page/en/the-studio.php
+++ page/en/the-studio.php
@@ -1,19 +1,21 @@
-<svg class="motio-vertical"><use xlink:href="/picture/sprite.svg#logo-motio-vertical" /></svg>
 <section class="row column expanded black the-studio-intro">
 	<div class="row">
-		<div class="column large-6 large-offset-2">
+		<div class="column medium-8 medium-offset-2 large-6">
 			<h1>Studio MOTIO is a France-based interactive design and creative web development studio, from La Rochelle.</h1>
 		</div>
 	</div>
 	<div class="row">
-		<div class="column large-4 large-offset-6">
+		<div class="column medium-6 medium-offset-5 large-4 large-offset-6">
 			<p>We design and build bespoke digital solutions to provide an intuitive, modern and high-quality user experience.</p>
 		</div>
 	</div>
 </section>
+<div class="wrapper-motio-vertical">
+	<svg class="motio-vertical"><use xlink:href="/picture/sprite.svg#logo-motio-vertical" /></svg>
+</div>
 <section class="row column expanded white">
 	<div class="row">
-		<div class="column large-4 large-offset-6">
+		<div class="column medium-6 medium-offset-5 large-4 large-offset-6">
 			<h2>The studio</h2>
 			<p>Specialized in animations and web micro-interactions based on the principles of motion design, we work in collaboration with designers and local developers, perfectionists, meticulous and detail-oriented.</p>
 			<p>The user experience is at the heart of our business, which is why we use cutting-edge graphic technologies, which have already proved their worth and are now recognized as references in the web domain for their compatibility, performance and their fluidity.</p>
@@ -23,7 +25,7 @@
 </section>
 <section class="row column expanded black">
 	<div class="row">
-		<div class="column large-4 large-offset-6">
+		<div class="column medium-6 medium-offset-5 large-4 large-offset-6">
 			<h2>Our services</h2>
 			<dl>
 				<dt>Design</dt>
--- page/fr/le-studio.php
+++ page/fr/le-studio.php
@@ -1,19 +1,21 @@
-<svg class="motio-vertical"><use xlink:href="/picture/sprite.svg#logo-motio-vertical" /></svg>
 <section class="row column expanded black the-studio-intro">
 	<div class="row">
-		<div class="column large-6 large-offset-2">
+		<div class="column medium-8 medium-offset-2 large-6">
 			<h1>Studio MOTIO est un studio Français spécialisé dans le design interactif, le développement web et basé à La Rochelle.</h1>
 		</div>
 	</div>
 	<div class="row">
-		<div class="column large-4 large-offset-6">
+		<div class="column medium-6 medium-offset-5 large-4 large-offset-6">
 			<p>Nous concevons et réalisons des solutions digitales sur mesure, dans le but d'offrir une expérience utilisateur intuitive, moderne et de qualité.</p>
 		</div>
 	</div>
 </section>
+<div class="wrapper-motio-vertical">
+	<svg class="motio-vertical"><use xlink:href="/picture/sprite.svg#logo-motio-vertical" /></svg>
+</div>
 <section class="row column expanded white">
 	<div class="row">
-		<div class="column large-4 large-offset-6">
+		<div class="column medium-6 medium-offset-5 large-4 large-offset-6">
 			<h2>Le studio</h2>
 			<p>Spécialisé dans la création d'animations et micro-interactions web basées sur les principes du motion design, nous travaillons en collaboration avec des designers et des développeurs de proximité, perfectionnistes, pointilleux et soucieux du détail.</p>
 			<p>L'expérience utilisateur est au cœur de notre métier, c'est pourquoi nous utilisons des technologies graphiques de pointe, ayant déjà fait leur preuve et s'imposant aujourd'hui comme des références dans le domaine du web pour leur compatibilité, leurs performances et leur fluidité.</p>
@@ -23,7 +25,7 @@
 </section>
 <section class="row column expanded black">
 	<div class="row">
-		<div class="column large-4 large-offset-6">
+		<div class="column medium-6 medium-offset-5 large-4 large-offset-6">
 			<h2>Nos services</h2>
 			<dl>
 				<dt>Design</dt>
