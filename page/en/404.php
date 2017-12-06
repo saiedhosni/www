5c2b02bca96cc23c5b45047105591cf47dc82365
Xavier Foucrier <xavier.foucrier@gmail.com>
2017-12-06 14:55:06 +0100

    Responsive 404 page (base content)

--- page/en/404.php
+++ page/en/404.php
@@ -1,1 +1,19 @@
-404
+<section class="row column expanded white">
+	<div class="row align-center">
+		<div class="column medium-9 large-8">
+			<h1>Oops!</h1>
+			<strong>404</strong>
+		</div>
+	</div>
+	<div class="row align-center collapse">
+		<div class="column medium-9 large-8">
+			<div class="row">
+				<div class="column small-12 large-8">
+					<p>The page you are looking for was not found: it may have been renamed, moved, or deleted.</p>
+					<p><a href="/" class="underline button"><svg><use xlink:href="/picture/sprite.svg#arrow-right" /></svg>Go back home</a></p>
+				</div>
+				<div class="column small-12 large-4"><div>
+			</div>
+		</div>
+	</div>
+</section>
--- page/fr/404.php
+++ page/fr/404.php
@@ -1,1 +1,19 @@
-404
+<section class="row column expanded white">
+	<div class="row align-center">
+		<div class="column medium-9 large-8">
+			<h1>Oops!</h1>
+			<strong>404</strong>
+		</div>
+	</div>
+	<div class="row align-center collapse">
+		<div class="column medium-9 large-8">
+			<div class="row">
+				<div class="column small-12 large-8">
+					<p>La page que vous recherchez est introuvable : elle a peut être été renommée, déplacée ou supprimée.</p>
+					<p><a href="/" class="underline button"><svg><use xlink:href="/picture/sprite.svg#arrow-right" /></svg>Rejoindre l'accueil</a></p>
+				</div>
+				<div class="column small-12 large-4"><div>
+			</div>
+		</div>
+	</div>
+</section>
