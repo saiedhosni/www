aaac467c1ee51155f22870a6239d35318db46143
Xavier Foucrier <xavier.foucrier@gmail.com>
2017-12-11 23:23:55 +0100

    Updating 404 content text

--- page/en/404.php
+++ page/en/404.php
@@ -2,17 +2,17 @@
 	<div class="row align-center">
 		<div class="column medium-9 large-8">
 			<h1>Oops!</h1>
-			<strong>404</strong>
+			<strong>It seems you're lost..</strong>
 		</div>
 	</div>
 	<div class="row align-center collapse">
 		<div class="column medium-9 large-8">
 			<div class="row">
-				<div class="column small-12 large-8">
-					<p>The page you are looking for was not found: it may have been renamed, moved, or deleted.</p>
+				<div class="column small-12 large-7">
+					<p>The page you are looking for was not found: it may have been renamed, moved, or deleted. We will conduct a small investigation to try to find this page, meanwhile, go enjoy a small coffee on terraces in front of the harbor <span class="emoji">☕</span>.</p>
 					<p><a href="/" class="underline button"><svg><use xlink:href="/picture/sprite.svg#arrow-right" /></svg>Go back home</a></p>
 				</div>
-				<div class="column small-12 large-4"><div>
+				<div class="column small-12 large-5"></div>
 			</div>
 		</div>
 	</div>
