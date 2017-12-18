e499947acf8f44012392d2bc285a969a8b548505
Xavier Foucrier <xavier.foucrier@gmail.com>
2017-12-18 15:02:07 +0100

    404 illustration tween

--- javascript/script.js
+++ javascript/script.js
@@ -226,4 +226,53 @@
 		menuCircle.play();
 		menuSteam.generate().play();
 	});
+
+	// 404 illustration tween
+	const illustration = document.querySelector('.illustration-404');
+
+	// checks if the illustration is present on the page
+	if (illustration != null) {
+
+		const illustrationOptions = {
+			parent: document.querySelector('.illustration-404'),
+			isForce3d: true
+		};
+
+		// fog tween
+		const fogShape = mojs.stagger(mojs.Shape);
+		const fog = new fogShape(
+			mojs.helpers.extend({
+				quantifier: 40,
+				shape: 'line',
+				stroke: colors.fog,
+				strokeWidth: 'rand(1.5, 3)',
+				strokeDasharray: '100%',
+				strokeDashoffset: { '-100%' : '100%' },
+				x: { 'rand(-250, 250)' : 'rand(-250, 250)' },
+				y: 'rand(-40, 60)',
+				opacity: { 0.8 : 0 },
+				radius: 'rand(20, 60)',
+				duration: 500,
+				repeat: 100,
+				delay: 'rand(0, 2000)',
+				isShowEnd: false,
+				speed: 0.03
+			}, illustrationOptions)
+		).play();
+
+		// sun tween
+		let sun = new mojs.Shape(
+			mojs.helpers.extend({
+				shape: 'circle',
+				fill: colors.warning,
+				left: '80%',
+				top: '20%',
+				radius: { 18 : 20 },
+				opacity: { 0.2 : 0.3 },
+				duration: 4000,
+				repeat: 3000,
+				isYoyo: true
+			}, illustrationOptions)
+		).play();
+	}
 })();
