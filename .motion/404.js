c83701c8d1864dd3766385bfc2cabec16d33efab
Xavier Foucrier <xavier.foucrier@gmail.com>
2017-12-18 10:45:35 +0100

    Smooth fog

--- .motion/404.js
+++ .motion/404.js
@@ -18,14 +18,38 @@
 		delay: 0
 	});
 
-	/*// defines the base options
+	// defines the base options
 	const options = {
 		parent: document.querySelector('#illustration-404'),
 		isForce3d: true
 	};
 
+	// fog
+	const fogShape = mojs.stagger(mojs.Shape);
+	const fog = new fogShape(
+		mojs.helpers.extend({
+			quantifier: 40,
+			shape: 'line',
+			stroke: colors.fog,
+			strokeWidth: 'rand(1.5, 3)',
+			strokeDasharray: '100%',
+			strokeDashoffset: { '-100%' : '100%' },
+			x: { 'rand(-250, 250)' : 'rand(-250, 250)' },
+			y: 'rand(-40, 60)',
+			opacity: { 1 : 0 },
+			radius: 'rand(20, 60)',
+			duration: 500,
+			delay: 'rand(0, 2000)',
+			isShowEnd: false,
+			repeat: 100,
+			speed: 0.02
+		}, options)
+	);
+
 	// adds shapes to the timeline
-	timeline.add();
+	timeline.add(
+		fog
+	);
 
 	// creates the player
 	new MojsPlayer({
