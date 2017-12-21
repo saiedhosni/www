87ab0fb1f7f7cb7ad29bae3721033767d16f6d82
Xavier Foucrier <xavier.foucrier@gmail.com>
2017-12-21 09:58:03 +0100

    Base animation for the motio letters

--- .motion/logo.js
+++ .motion/logo.js
@@ -26,8 +26,112 @@
 	let time = 500;
 	let delay = 100;
 
+	// defines the base options
+	const options = {
+		fill: 'transparent',
+		stroke: colors.base,
+		duration: time,
+		easing: mojs.easing.path('M0,100 C50,100 50,67.578125 50,50 C50,32.421875 50,0 100,0'),
+		isForce3d: true
+	};
+
+	// motio "m" tween
+	let motion_m_vertical = new mojs.Html(
+		mojs.helpers.extend({
+			el: '#motio-m-vertical',
+			strokeDasharray: 80,
+			strokeDashoffset: { '-80' : 0 }
+		}, options)
+	);
+
+	let motion_m_arc_1 = new mojs.Html(
+		mojs.helpers.extend({
+			el: '#motio-m-arc-1',
+			strokeDasharray: 98,
+			strokeDashoffset: { 98 : 0 },
+			delay: delay
+		}, options)
+	);
+
+	let motion_m_arc_2 = new mojs.Html(
+		mojs.helpers.extend({
+			el: '#motio-m-arc-2',
+			strokeDasharray: 119,
+			strokeDashoffset: { 119 : 0 },
+			delay: delay * 2
+		}, options)
+	);
+
+	// motio "o" tween
+	let motion_o_first = new mojs.Html(
+		mojs.helpers.extend({
+			el: '#motio-o-first',
+			strokeDasharray: 138,
+			strokeDashoffset: { 138 : 0 },
+			delay: delay * 3
+		}, options)
+	);
+
+	// motio "t" tween
+	let motio_t_horizontal = new mojs.Html(
+		mojs.helpers.extend({
+			el: '#motio-t-horizontal',
+			strokeDasharray: 54,
+			strokeDashoffset: { 54 : 0 },
+			delay: delay * 4
+		}, options)
+	);
+
+	let motio_t_vertical = new mojs.Html(
+		mojs.helpers.extend({
+			el: '#motio-t-vertical',
+			strokeDasharray: 113,
+			strokeDashoffset: { '-113' : 0 },
+			delay: delay * 5
+		}, options)
+	);
+
+	// motion "i" tween
+	let motio_i_vertical = new mojs.Html(
+		mojs.helpers.extend({
+			el: '#motio-i-vertical',
+			strokeDasharray: 40,
+			strokeDashoffset: { '-40' : 0 },
+			delay: delay * 6
+		}, options)
+	);
+
+	let motio_i_dot = new mojs.Html(
+		mojs.helpers.extend({
+			el: '#motio-i-dot',
+			r: { 0 : 12.633 },
+			delay: delay * 7,
+			fill: '#000'
+		}, options)
+	);
+
+	// motion "o" tween
+	let motio_o_last = new mojs.Html(
+		mojs.helpers.extend({
+			el: '#motio-o-last',
+			strokeDasharray: 138,
+			strokeDashoffset: { '-138' : 0 },
+			delay: delay * 8
+		}, options)
+	);
+
 	// adds shapes to the timeline
-	timeline.add();
+	timeline.add(
+		motion_m_vertical,
+		motion_m_arc_1,
+		motion_m_arc_2,
+		motion_o_first,
+		motio_t_horizontal,
+		motio_t_vertical,
+		motio_i_vertical,
+		motio_i_dot,
+		motio_o_last
+	);
 
 	// creates the player
 	new MojsPlayer({
