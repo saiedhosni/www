babe168e1bd8b3a73787c4e4906363769f495bf1
Xavier Foucrier <xavier.foucrier@gmail.com>
2018-01-24 09:45:40 +0100

    Centers the section vertically

--- page/en/404.php
+++ page/en/404.php
@@ -1,20 +1,22 @@
-<section class="row column expanded white">
-	<div class="row align-center">
-		<div class="column medium-9 large-8">
-			<h1>Oops!</h1>
-			<strong>It seems you're lost..</strong>
+<section class="row align-middle expanded white">
+	<div class="column">
+		<div class="row align-center">
+			<div class="column medium-9 large-8">
+				<h1>Oops!</h1>
+				<strong>It seems you're lost..</strong>
+			</div>
 		</div>
-	</div>
-	<div class="row align-center collapse">
-		<div class="column medium-9 large-8">
-			<div class="row">
-				<div class="column small-12 large-7">
-					<p>The page you are looking for was not found: it may have been renamed, moved, or deleted. We will conduct a small investigation to try to find this page, meanwhile, go enjoy a small coffee on terraces in front of the harbor.</p>
-					<p><a href="/" class="underline button"><svg><use xlink:href="/picture/sprite.svg#arrow-right" /></svg>Go back home</a></p>
-				</div>
-				<div class="column small-12 large-5">
-					<div class="illustration-404">
-						<svg viewBox="0 0 500 260"><use xlink:href="/picture/sprite.svg#la-rochelle" /></svg>
+		<div class="row align-center collapse">
+			<div class="column medium-9 large-8">
+				<div class="row">
+					<div class="column small-12 large-7">
+						<p>The page you are looking for was not found: it may have been renamed, moved, or deleted. We will conduct a small investigation to try to find this page, meanwhile, go enjoy a small coffee on terraces in front of the harbor.</p>
+						<p><a href="/" class="underline button"><svg><use xlink:href="/picture/sprite.svg#arrow-right" /></svg>Go back home</a></p>
+					</div>
+					<div class="column small-12 large-5">
+						<div class="illustration-404">
+							<svg viewBox="0 0 500 260"><use xlink:href="/picture/sprite.svg#la-rochelle" /></svg>
+						</div>
 					</div>
 				</div>
 			</div>
--- page/fr/404.php
+++ page/fr/404.php
@@ -1,20 +1,22 @@
-<section class="row column expanded white">
-	<div class="row align-center">
-		<div class="column medium-9 large-8">
-			<h1>Oops!</h1>
-			<strong>Il semble que vous soyez perdu..</strong>
+<section class="row align-middle expanded white">
+	<div class="column">
+		<div class="row align-center">
+			<div class="column medium-9 large-8">
+				<h1>Oops!</h1>
+				<strong>Il semble que vous soyez perdu..</strong>
+			</div>
 		</div>
-	</div>
-	<div class="row align-center collapse">
-		<div class="column medium-9 large-8">
-			<div class="row">
-				<div class="column small-12 large-7">
-					<p>La page que vous recherchez est introuvable : elle a peut être été renommée, déplacée ou supprimée. Nous allons menez notre petite enquête pour essayer de retrouver cette page, en attendant, allez déguster un petit café sur les terrases en face du port.</p>
-					<p><a href="/" class="underline button"><svg><use xlink:href="/picture/sprite.svg#arrow-right" /></svg>Rejoindre l'accueil</a></p>
-				</div>
-				<div class="column small-12 large-5">
-					<div class="illustration-404">
-						<svg viewBox="0 0 500 260"><use xlink:href="/picture/sprite.svg#la-rochelle" /></svg>
+		<div class="row align-center collapse">
+			<div class="column medium-9 large-8">
+				<div class="row">
+					<div class="column small-12 large-7">
+						<p>La page que vous recherchez est introuvable : elle a peut être été renommée, déplacée ou supprimée. Nous allons menez notre petite enquête pour essayer de retrouver cette page, en attendant, allez déguster un petit café sur les terrases en face du port.</p>
+						<p><a href="/" class="underline button"><svg><use xlink:href="/picture/sprite.svg#arrow-right" /></svg>Rejoindre l'accueil</a></p>
+					</div>
+					<div class="column small-12 large-5">
+						<div class="illustration-404">
+							<svg viewBox="0 0 500 260"><use xlink:href="/picture/sprite.svg#la-rochelle" /></svg>
+						</div>
 					</div>
 				</div>
 			</div>
