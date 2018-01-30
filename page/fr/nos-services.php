f119632d4256b4169068e0b4836956732811c68e
Xavier Foucrier <xavier.foucrier@gmail.com>
2018-01-30 17:46:49 +0100

    Fixes layout

--- page/fr/nos-services.php
+++ page/fr/nos-services.php
@@ -1,24 +1,24 @@
 <section class="white intro">
 	<div class="row">
-		<div class="column medium-8 medium-offset-5 large-6">
-			<h1 data-emergence>L'art de l'animation et de l'interaction sur mesure, au bon endroit et au bon moment !</h1>
+		<div class="column align-self-top medium-7 medium-offset-4 large-offset-4">
+			<h1 data-emergence>L'art de la création, l'animation et de l'interaction sur mesure, au bon endroit et au bon moment, notre spécialité !</h1>
 		</div>
 	</div>
 	<div class="row">
-		<div class="column medium-6 medium-offset-5 large-4 large-offset-2">
-			<p data-emergence>Nous pensons et construisons votre projet autour de vos exigences, en se focalisant sur ce qui est important à vos yeux. Afin que l'imaginaire prenne vie.</p>
+		<div class="column align-self-bottom medium-6 medium-offset-2 large-4">
+			<p data-emergence>Nous pensons et construisons votre projet autour de vos exigences, en se focalisant sur ce qui est important. Afin que l'imaginaire prenne vie.</p>
 		</div>
 	</div>
 </section>
 <section class="black expanded">
 	<div class="row">
-		<div class="column medium-8 medium-offset-2">
+		<div class="column medium-6 medium-offset-2">
 			<h2 data-emergence>Nos services</h2>
 			<p data-emergence>Le studio réalise des solutions digitales interactives. Nous portons une attention toute particulières aux nouvelles technologies, à l'open source ainsi qu'à la recherche et au développement de nouveaux produits.</p>
 		</div>
 	</div>
 	<div class="row">
-		<div class="column medium-offset-2">
+		<div class="column medium-6 medium-offset-2 large-4">
 			<dl>
 				<dt data-emergence>Design</dt>
 				<dd data-emergence>Logotype</dd>
@@ -27,18 +27,12 @@
 				<dd data-emergence>Webdesign (UI)</dd>
 				<dd data-emergence>Stratégie digitale</dd>
 			</dl>
-		</div>
-		<div class="column small-12 medium-6">
 			<dl>
 				<dt data-emergence>Motion</dt>
 				<dd data-emergence>Micro-interactions</dd>
 				<dd data-emergence>Expérience utilisateur (UX)</dd>
 				<dd data-emergence>Design interactif pour le web</dd>
 			</dl>
-		</div>
-	</div>
-	<div class="row">
-		<div class="column medium-offset-2">
 			<dl>
 				<dt data-emergence>Développement</dt>
 				<dd data-emergence>Site vitrine</dd>
