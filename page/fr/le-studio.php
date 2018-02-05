df0ce14ca0f934803a98b50ddcaf9ea8a851aef4
Xavier Foucrier <xavier.foucrier@gmail.com>
2018-02-05 11:33:49 +0100

    Vertical motio cutting and positioning

--- page/fr/le-studio.php
+++ page/fr/le-studio.php
@@ -11,7 +11,23 @@
 	</div>
 </section>
 <div class="wrapper-motio-vertical hide-for-small-only">
-	<svg class="motio-vertical"><use xlink:href="/picture/sprite.svg#logo-motio-vertical" /></svg>
+	<div class="motio-vertical">
+		<div class="vertical letter-o-last">
+			<svg viewBox="0 0 370 251.721"><path stroke-width="65.502" d="M244.16 218.97c51.423-.011 93.089-41.68 93.089-93.13 0-51.408-41.681-93.089-93.1-93.089-51.424 0-93.084 41.681-93.084 93.089v.021"/></svg>
+		</div>
+		<div class="vertical letter-i">
+			<svg viewBox="0 0 370 83.57"><line x1="116.399" x2="244.849" y1="42.241" y2="42.241" stroke-width="68.777"/><circle cx="41.784" cy="41.785" r="41.784"/></svg>
+		</div>
+		<div class="vertical letter-t">
+			<svg viewBox="0 0 370 170.94"><line x1="146.646" x2="146.646" y1="170.944" y2=".066" stroke-width="60.262"/><path stroke-width="65.502" d="M41.245 92.373h218.279c41.819-.195 70.256-16.79 76.269-51.787 2.43-14.226 1.085-26.616 1.085-40.586"/></svg>
+		</div>
+		<div class="vertical letter-o">
+			<svg viewBox="0 0 370 251.719"><path stroke-width="65.502" d="M337.249 125.844c0-51.423-41.675-93.093-93.098-93.093-51.441 0-93.09 41.68-93.09 93.13 0 51.424 41.675 93.087 93.09 93.087h.015"/></svg>
+		</div>
+		<div class="vertical letter-m">
+			<svg viewBox="0 0 370 373.25"><line x1="121.49" x2="370" y1="340.5" y2="340.5" stroke-width="65.502"/><path stroke-width="65.502" d="M370 337.285H227.704c-34.904-.309-73.811-25.31-73.811-76.3 0-50.128 38.814-75.014 73.375-75.014l88.54-.016"/><path stroke-width="65.502" d="M315.809 184.087h-88.104c-34.904-.307-73.811-25.312-73.811-76.299 0-50.129 38.814-75.015 73.375-75.015L370 32.752"/></svg>
+		</div>
+	</div>
 </div>
 <section class="white">
 	<div class="row">
