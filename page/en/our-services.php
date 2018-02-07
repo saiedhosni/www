46c6c08851d4548e490252e0b6023e51ace8a8df
Xavier Foucrier <xavier.foucrier@gmail.com>
2018-02-07 19:06:55 +0100

    Completes the our services page (english version)

--- page/en/our-services.php
+++ page/en/our-services.php
@@ -1,1 +1,45 @@
-our services
+<section class="white intro">
+	<div class="row">
+		<div class="column align-self-top medium-7 medium-offset-4 large-offset-4 text-right">
+			<h1 data-emergence>The art of creation, animation and interaction tailored to the right place and the right time, our specialty!</h1>
+		</div>
+	</div>
+	<div class="row">
+		<div class="column align-self-bottom medium-6 medium-offset-2 large-4">
+			<p data-emergence>We think and build your project around your requirements, focusing on what's important. So that the imagination comes to life.</p>
+		</div>
+	</div>
+</section>
+<section class="black expanded">
+	<div class="row">
+		<div class="column medium-6 medium-offset-2">
+			<h2 data-emergence>Our services</h2>
+			<p data-emergence>The studio produces interactive digital solutions. We pay special attention to new technologies, open source and research and development of new products.</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="column medium-6 medium-offset-2 large-4">
+			<dl>
+				<dt data-emergence>Design</dt>
+				<dd data-emergence>Logotype</dd>
+				<dd data-emergence>Branding</dd>
+				<dd data-emergence>Visual identity</dd>
+				<dd data-emergence>Webdesign (UI)</dd>
+				<dd data-emergence>Digital strategy</dd>
+			</dl>
+			<dl>
+				<dt data-emergence>Motion</dt>
+				<dd data-emergence>Micro-interactions</dd>
+				<dd data-emergence>User experience (UX)</dd>
+				<dd data-emergence>Interactive design for the web</dd>
+			</dl>
+			<dl>
+				<dt data-emergence>Development</dt>
+				<dd data-emergence>Showcase site</dd>
+				<dd data-emergence>Custom tailored platform</dd>
+				<dd data-emergence>E-Shop</dd>
+				<dd data-emergence>Optimization & SEO</dd>
+			</dl>
+		</div>
+	</div>
+</section>
