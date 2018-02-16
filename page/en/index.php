de68c725003891cfddef8b9df91ee2428a3d5bea
Xavier Foucrier <xavier.foucrier@gmail.com>
2018-02-16 10:32:14 +0100

    Fixes blending layer issue
    Chromium has a blending issue with negative z-index: updating all z-index to be positive.

--- page/en/index.php
+++ page/en/index.php
@@ -1,9 +1,11 @@
-<div class="slider">
-	<svg class="motio-o-home"><use xlink:href="/picture/sprite.svg#logo-motio-o" /></svg>
-	<div>
-		<h1>France-based interactive design & creative web studio</h1>
-		<a href="/en/the-studio" class="button underline">
-			<span class="arrow"></span>Discover the studio
-		</a>
+<section class="black">
+	<div class="slider">
+		<svg class="motio-o-home"><use xlink:href="/picture/sprite.svg#logo-motio-o" /></svg>
+		<div>
+			<h1>France-based interactive design & creative web studio</h1>
+			<a href="/en/the-studio" class="button underline">
+				<span class="arrow"></span>Discover the studio
+			</a>
+		</div>
 	</div>
-</div>
+</section>
--- page/fr/index.php
+++ page/fr/index.php
@@ -1,9 +1,11 @@
-<div class="slider">
-	<svg class="motio-o-home"><use xlink:href="/picture/sprite.svg#logo-motio-o" /></svg>
-	<div>
-		<h1>Studio de design interactif et de développement web</h1>
-		<a href="le-studio" class="button underline">
-			<span class="arrow"></span>Découvrir le studio
-		</a>
+<section class="black">
+	<div class="slider">
+		<svg class="motio-o-home"><use xlink:href="/picture/sprite.svg#logo-motio-o" /></svg>
+		<div>
+			<h1>Studio de design interactif et de développement web</h1>
+			<a href="le-studio" class="button underline">
+				<span class="arrow"></span>Découvrir le studio
+			</a>
+		</div>
 	</div>
-</div>
+</section>
