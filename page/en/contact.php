b4445f8b554769fd80cd3b10b3908d4597b5c953
Xavier Foucrier <xavier.foucrier@gmail.com>
2018-03-28 15:39:42 +0200

    Contact page transition optimization (english)

--- page/en/contact.php
+++ page/en/contact.php
@@ -1,34 +1,39 @@
 <section class="white">
-	<svg class="motio-o-contact"><use xlink:href="/picture/sprite.svg#logo-motio-o" /></svg>
+	<!-- <svg class="motio-o-contact"><use xlink:href="/picture/sprite.svg#logo-motio-o" /></svg> -->
 	<div class="row align-center">
 		<div class="column medium-9 large-8">
-			<p data-emergence="hidden">Want to entrust us with your project, launch a new website or build a memorable brand identity, leave us a note. We would love to work with you!</p>
+			<h1 data-emergence="visible">Brief-us!</h1>
+			<p data-emergence="hidden" class="shift">Want to entrust us with your project, launch a new website or build a memorable brand identity, leave us a note. We would love to work with you!</p>
 		</div>
 	</div>
 	<div class="row align-center">
 		<div class="column medium-9 large-8">
-			<p data-emergence="hidden"><strong class="type">Hello, what are your needs?</strong></p>
-			<form method="post" data-emergence="hidden">
-				<textarea required="required" autocomplete="off"></textarea>
-				<span class="progress"></span>
-				<button name="submit" class="button post"><span class="arrow"></span>Post this message</button>
-				<strong class="message warning missing-mail">Don't forget to let your mail address with your message</strong>
-				<strong class="message warning bad-request">Oops, your message could not be sent, please try again in a few moments</strong>
-				<strong class="message pending-request">Sending in progress..</strong>
-				<strong class="message message-sent">Message sent. We will answer you soon<span class="emoji">✌</span></strong>
+			<p data-emergence="hidden" class="shift"><strong class="type">Hello, what are your needs?</strong></p>
+			<form method="post">
+				<div data-emergence="hidden" class="shift">
+					<textarea required="required" autocomplete="off"></textarea>
+				</div>
+				<div data-emergence="hidden" class="shift">
+					<span class="progress"></span>
+					<button name="submit" class="button post"><span class="arrow"></span>Post this message</button>
+					<strong class="message warning missing-mail">Don't forget to let your mail address with your message</strong>
+					<strong class="message warning bad-request">Oops, your message could not be sent, please try again in a few moments</strong>
+					<strong class="message pending-request">Sending in progress..</strong>
+					<strong class="message message-sent">Message sent. We will answer you soon<span class="emoji">✌</span></strong>
+				</div>
 			</form>
 		</div>
 	</div>
 	<div class="row align-center">
 		<div class="column small-12 medium-9 large-4">
-			<p data-emergence="hidden"><strong>Collaboration</strong></p>
-			<p class="no-margin" data-emergence="hidden">We are always very interested in building new partnerships. You have talent and you want to share it with us, join the collective!</p>
-			<p class="no-margin" data-emergence="hidden"><small><a href="mailto:collective@studiomotio.com" target="_blank" class="highlight">collective@studiomotio.com</a></small></p>
+			<p data-emergence="hidden" class="shift"><strong>Collaboration</strong></p>
+			<p class="no-margin shift" data-emergence="hidden">We are always very interested in building new partnerships. You have talent and you want to share it with us, join the collective!</p>
+			<p class="no-margin shift" data-emergence="hidden"><small><a href="mailto:collective@studiomotio.com" target="_blank" class="highlight">collective@studiomotio.com</a></small></p>
 		</div>
 		<div class="column small-12 medium-9 large-4">
-			<p data-emergence="hidden"><strong>Jobs</strong></p>
-			<p class="no-margin" data-emergence="hidden">The studio does not host trainees at the moment. Still want to show us what you talents and what you are capable of? Send us your resume!</p>
-			<p class="no-margin" data-emergence="hidden"><small><a href="mailto:jobs@studiomotio.com" target="_blank" class="highlight">jobs@studiomotio.com</a></small></p>
+			<p data-emergence="hidden" class="shift"><strong>Jobs</strong></p>
+			<p class="no-margin shift" data-emergence="hidden">The studio does not host trainees at the moment. Still want to show us what you talents and what you are capable of? Send us your resume!</p>
+			<p class="no-margin shift" data-emergence="hidden"><small><a href="mailto:jobs@studiomotio.com" target="_blank" class="highlight">jobs@studiomotio.com</a></small></p>
 		</div>
 	</div>
 </section>
