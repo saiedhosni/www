a33ec3233dd1d13dbbfdb37f8581acba158bf111
Xavier Foucrier <xavier.foucrier@gmail.com>
2018-04-26 16:35:17 +0200

    Fixes 404 back homepage link in the english version

--- page/en/404.php
+++ page/en/404.php
@@ -11,7 +11,7 @@
 				<div class="row align-justify collapse">
 					<div class="column small-12 large-6">
 						<p data-emergence="hidden" class="shift">The page you are looking for was not found: it may have been renamed, moved, or deleted. We will conduct a small investigation to try to find this page, meanwhile, go enjoy a small coffee on terraces in front of the harbor.</p>
-						<p data-emergence="hidden" class="shift"><a href="/" class="button"><span class="arrow"></span>Go back home</a></p>
+						<p data-emergence="hidden" class="shift"><a href="/en" class="button"><span class="arrow"></span>Go back home</a></p>
 					</div>
 					<div class="column small-12 large-5">
 						<div class="illustration-404 shift" data-emergence="hidden">
