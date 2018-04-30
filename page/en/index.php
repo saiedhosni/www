b2dcfeedb2b000c1527fec724483cc02d1c17757
Xavier Foucrier <xavier.foucrier@gmail.com>
2018-04-30 17:14:29 +0200

    Fixes svg stacking position

--- page/en/index.php
+++ page/en/index.php
@@ -1,13 +1,13 @@
 <section class="black">
 	<div class="slider">
+		<svg class="motio-o-home" viewBox="0 0 100 100">
+			<path class="isolation" fill="none" stroke-width="0" d="M12 50c0-21 17-38 38-38s38 17 38 38-17 38-38 38"/>
+		</svg>
 		<div>
 			<h1 data-emergence="hidden">France-based interactive design & creative web studio</h1>
 			<a href="/en/the-studio" class="button shift" data-emergence="hidden" data-dot="contrast">
 				<span class="arrow"></span>Discover the studio
 			</a>
 		</div>
-		<svg class="motio-o-home" viewBox="0 0 100 100">
-			<path class="isolation" fill="none" stroke-width="0" d="M12 50c0-21 17-38 38-38s38 17 38 38-17 38-38 38"/>
-		</svg>
 	</div>
 </section>
