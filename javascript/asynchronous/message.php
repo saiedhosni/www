96a5bf660171003ad4a3ab2166d2a22bc76b9de0
Xavier Foucrier <xavier.foucrier@gmail.com>
2018-05-09 12:06:07 +0200

    Strengthens contact form security
    Prevents the contact form from sending email when the message does not contains a valid email address in backend. This also prevent spam receip.

--- javascript/asynchronous/message.php
+++ javascript/asynchronous/message.php
@@ -8,6 +8,11 @@
 		return;
 	}
 
+	// email checkpoint
+	if (preg_match('/(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/', $_POST['message']) == false) {
+		return;
+	}
+
 	// includes the site configuration
 	require '../../configuration.php';
 
