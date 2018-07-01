efb033f51ae56d1d909562d61403514ea1a9c3c5
Xavier Foucrier <xavier.foucrier@gmail.com>
2018-07-01 17:01:18 +0200

    Source folder
    This folder will contains all javascript source files.

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
 	mode: 'development',
 	watch: true,
-	entry: './javascript/script.js',
+	entry: './javascript/src/script.js',
 	output: {
 		filename: 'app.js',
 		path: path.resolve(__dirname, 'javascript')
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
 	mode: 'production',
-	entry: './javascript/script.js',
+	entry: './javascript/src/script.js',
 	output: {
 		filename: 'app.min.js',
 		path: path.resolve(__dirname, 'javascript')
