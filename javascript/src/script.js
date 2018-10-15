f21a5d8e02ec7e849cb4fb02eca7fd5ca7abc748
Xavier Foucrier <xavier.foucrier@gmail.com>
2018-10-15 11:12:13 +0200

    Short IIFE syntax
    This closes #47.

--- javascript/src/script.js
+++ javascript/src/script.js
@@ -7,29 +7,26 @@ import * as preload from './preload.js';
 import * as scroll from './scroll.js';
 import {dot} from './dot.js';
 
-(function() {
+// web developer console signature
+console.log('%cMade with ❤︎️ by Studio MOTIO — Interactive design and creative web studio from La Rochelle — studiomotio.com', 'background:#000;color:#fff;padding:0.5em 1em;line-height:2;');
 
-	// web developer console signature
-	console.log('%cMade with ❤︎️ by Studio MOTIO — Interactive design and creative web studio from La Rochelle — studiomotio.com', 'background:#000;color:#fff;padding:0.5em 1em;line-height:2;');
+// inits the barba views
+view.init();
 
-	// inits the barba views
-	view.init();
+// inits the mobile menu
+menu.init();
 
-	// inits the mobile menu
-	menu.init();
+// binds some elements
+event.bindLogos();
+event.bindFooter();
+event.bindTabKey();
+event.bindLinks();
 
-	// binds some elements
-	event.bindLogos();
-	event.bindFooter();
-	event.bindTabKey();
-	event.bindLinks();
+// inits the dot cursor
+dot.init();
 
-	// inits the dot cursor
-	dot.init();
+// inits the smooth scroll
+scroll.init();
 
-	// inits the smooth scroll
-	scroll.init();
-
-	// inits the site preload animation
-	preload.init();
-})();
+// inits the site preload animation
+preload.init();
