4f9d97fcd1bcbd6393719a59e1fe64f9105bb86f
Xavier Foucrier <xavier.foucrier@gmail.com>
2018-10-15 11:34:34 +0200

    Renames the main entry point

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
 	mode: 'development',
 	watch: true,
-	entry: './javascript/src/script.js',
+	entry: './javascript/src/index.js',
 	output: {
 		filename: 'app.js',
 		path: path.resolve(__dirname, 'javascript')
