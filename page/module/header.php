54c672e15c5cd12022d3173256a4c39d5bf02808
Xavier Foucrier <xavier.foucrier@gmail.com>
2018-11-08 18:15:08 +0100

    Fix unsafe cross-origin destinations
    This fixes #54.

--- page/module/header.php
+++ page/module/header.php
@@ -39,9 +39,9 @@
           <li class="<?php echo LANGUAGE == 'en' ? 'active' : ''; ?>"><a href="/en" class="index <?php echo LANGUAGE == 'en' ? '' : 'no-barba'; ?>">En</a></li>
         </ul>
         <ul class="social">
-          <li><a href="https://www.facebook.com/studiomotio" target="_blank"><svg><use xlink:href="/picture/sprite.svg#logo-facebook" /></svg></a></li>
-          <li><a href="https://www.twitter.com/studiomotio" target="_blank"><svg><use xlink:href="/picture/sprite.svg#logo-twitter" /></svg></a></li>
-          <li><a href="https://www.instagram.com/studiomotio" target="_blank"><svg><use xlink:href="/picture/sprite.svg#logo-instagram" /></svg></a></li>
+          <li><a href="https://www.facebook.com/studiomotio" target="_blank" rel="noopener"><svg><use xlink:href="/picture/sprite.svg#logo-facebook" /></svg></a></li>
+          <li><a href="https://www.twitter.com/studiomotio" target="_blank" rel="noopener"><svg><use xlink:href="/picture/sprite.svg#logo-twitter" /></svg></a></li>
+          <li><a href="https://www.instagram.com/studiomotio" target="_blank" rel="noopener"><svg><use xlink:href="/picture/sprite.svg#logo-instagram" /></svg></a></li>
         </ul>
       </li>
     </ul>
--- page/module/media.php
+++ page/module/media.php
@@ -1,11 +1,11 @@
 <div class="media hide-for-small-only">
-  <a href="https://www.facebook.com/studiomotio" target="_blank">
+  <a href="https://www.facebook.com/studiomotio" target="_blank" rel="noopener">
     <svg><use xlink:href="/picture/sprite.svg#logo-facebook" /></svg>
   </a>
-  <a href="https://www.twitter.com/studiomotio" target="_blank">
+  <a href="https://www.twitter.com/studiomotio" target="_blank" rel="noopener">
     <svg><use xlink:href="/picture/sprite.svg#logo-twitter" /></svg>
   </a>
-  <a href="https://www.instagram.com/studiomotio" target="_blank">
+  <a href="https://www.instagram.com/studiomotio" target="_blank" rel="noopener">
     <svg><use xlink:href="/picture/sprite.svg#logo-instagram" /></svg>
   </a>
 </div>
