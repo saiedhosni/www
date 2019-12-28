d4dc6915c349e68f06e07176a87c4d590754934f
Xavier Foucrier <xavier.foucrier@gmail.com>
2019-12-28 17:10:17 +0100

    Improve accessibility

--- page/layout/media.php
+++ page/layout/media.php
@@ -1,11 +1,11 @@
 <div class="media hide-for-small-only">
-  <a href="https://www.facebook.com/studiomotio" target="_blank" rel="noopener">
+  <a href="https://www.facebook.com/studiomotio" target="_blank" rel="noopener" aria-label="Facebook">
     <svg><use xlink:href="/picture/sprite.svg#logo-facebook" /></svg>
   </a>
-  <a href="https://www.twitter.com/studiomotio" target="_blank" rel="noopener">
+  <a href="https://www.twitter.com/studiomotio" target="_blank" rel="noopener" aria-label="Twitter">
     <svg><use xlink:href="/picture/sprite.svg#logo-twitter" /></svg>
   </a>
-  <a href="https://www.instagram.com/studiomotio" target="_blank" rel="noopener">
+  <a href="https://www.instagram.com/studiomotio" target="_blank" rel="noopener" aria-label="Instagram">
     <svg><use xlink:href="/picture/sprite.svg#logo-instagram" /></svg>
   </a>
 </div>
