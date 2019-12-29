1a8886848970f113c44e7f47818d63ec1bf5264e
Xavier Foucrier <xavier.foucrier@gmail.com>
2019-12-29 16:49:15 +0100

    Add the Cap Mystere logo

--- page/fr/nos-services.php
+++ page/fr/nos-services.php
@@ -58,6 +58,7 @@
   <div class="row">
     <div class="column medium-8 medium-offset-2 large-8">
       <ul class="row small-up-1 medium-up-2 large-up-3 references">
+        <li class="column" data-emergence="hidden"><svg viewBox="0 0 100 100"><use xlink:href="/picture/sprite.svg#logo-capmystere" /></svg></li>
         <li class="column" data-emergence="hidden"><svg viewBox="0 0 100 100"><use xlink:href="/picture/sprite.svg#logo-kosmo" /></svg></li>
         <li class="column" data-emergence="hidden"><svg viewBox="0 0 100 100"><use xlink:href="/picture/sprite.svg#logo-kyoso" /></svg></li>
         <li class="column" data-emergence="hidden"><svg viewBox="0 0 100 100"><use xlink:href="/picture/sprite.svg#logo-p2mv" /></svg></li>
